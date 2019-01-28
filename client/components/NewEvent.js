b63a9090bd2d59912b3f823b92a4a1402e1903b9
mattrsp <mattsnyc93@gmail.com>
2019-01-28 13:19:48 -0500

    added new event form

---
+++ client/components/NewEvent.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react'
+import EventForm from './EventForm'
+import {createOneEvent} from '../store/index'
+import {connect} from 'react-redux'
+
+class NewEvent extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      description: '',
+      location: '',
+      firstchoice: '',
+      secondchoice: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  handleSubmit(event, values) {
+    event.preventDefault()
+    this.setState({
+      values
+    })
+    const {name, description, location, firstchoice, secondchoice} = this.state
+    const newEvent = {
+      name,
+      description,
+      location
+    }
+    this.props.createOneEvent(newEvent)
+  }
+  render() {
+    return (
+      <EventForm
+        {...this.state}
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+      />
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    newUser: state.user
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    createOneEvent: newEvent => dispatch(createOneEvent(newEvent))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewEvent)
