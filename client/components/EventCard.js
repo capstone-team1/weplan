ce4f6d6a4081efbb376486174c05bd2467f26084
astaszew <astaszew@gmail.com>
2019-01-30 12:42:11 -0500

    added some buttons ui

--- client/components/EventCard.js
+++ client/components/EventCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Button} from 'react-bootstrap'
 
 const EventCard = props => {
   const {event} = props
@@ -8,6 +9,8 @@ const EventCard = props => {
       <p id="description">{event.description}</p>
       <p id="location">{event.location}</p>
       <p id="upvotes">{event.upvotes}</p>
+      <Button>upvote</Button>
+      <Button bsStyle="warning">downvote</Button>
     </div>
   )
 }
